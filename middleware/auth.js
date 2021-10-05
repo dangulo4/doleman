f5fccb4b511d1a5d699d08755c9b0d5d9f583917
Daniel Angulo <dangulo4@gmail.com>
2021-10-04 20:20:30 -0700

    added middleware for get logged in user auth

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+// Middleware function
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token')
+  // Check for valid token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization is denied' })
+  }
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    req.user = decoded.user
+    next()
+  } catch (err) {
+    res.status(401).json({ msg: 'Invalid Token' })
+  }
+}
