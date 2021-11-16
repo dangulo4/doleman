8aee8c5589c2857b494aa8083394878236b3ecdd
Daniel Angulo <dangulo4@gmail.com>
2021-11-15 21:05:50 -0800

    created lambda function to access database

--- lambda_functions/dolemancontact.js
+++ lambda_functions/dolemancontact.js
@@ -1,13 +1,15 @@
 const MongoClient = require('mongodb').MongoClient
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 const MONGODB_URI = process.env.MONGO_URI
 const DB_NAME = 'contactkeeper'
 
-let cacheDb = null
+let cachedDb = null
 
 const connectToDatabase = async (uri) => {
   // we can cache the access to our database to speed things up a bit
-  if (cacheDb) return cachedDb
+  if (cachedDb) return cachedDb
 
   const client = await MongoClient.connect(uri, {
     useUnifiedTopology: true,
@@ -36,5 +38,18 @@ module.exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
 
   const db = await connectionToDatabase(MONGODB_URI)
+  // Get token from header
+  const token = req.header('x-auth-token')
+  // Check for valid token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization is denied' })
+  }
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    req.user = decoded.user
+    next()
+  } catch (err) {
+    res.status(401).json({ msg: 'Invalid Token' })
+  }
   return queryDatabase(db)
 }
